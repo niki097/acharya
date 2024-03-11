9eea8fb26772ee23cb7aa7e370c95b25181ede89
niki097 <nikitasaini09@gmail.com>
2024-03-11 19:32:29 +0530

    d

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './style.css';
 import usa from '../assets/imgs/USA.svg';
+import email from '../assets/imgs/dashicons_email-alt2.svg';
 
 function Register() {
     const [selectedCountry, setSelectedCountry] = useState(null);
@@ -17,20 +18,20 @@ function Register() {
                 <p className='text-center'><span>Log in or Sign up</span></p>
                 <div className="input_number">
                     <span><img src={usa} alt="" /></span>
-                    <select name="country" value={selectedCountry} onChange={handleCountryChange}>
+                    <select className='bg-white mx-3' name="country" value={selectedCountry} onChange={handleCountryChange}>
                         <option value="US">+1</option>
                         <option value="ind">+91</option>
                         <option value="pak">+92</option>
                         <option value="aus">+3</option>
                     </select>
-                    <input placeholder='Enter Mobile Number' type="text" />
+                    <input className="text-md text-gray-900 dark:text-white" placeholder='Enter Mobile Number' type="text" />
                 </div>
-                <button>
+                <div className='send_otp_btn'> <button>
                     SEND OTP
-                </button>
+                </button></div>
                 <p><span>or</span></p>
-                <button><span><img src="" alt="" /></span>Continue with Email Id</button>
-                <p className=''>By signing up, you agree to our <a href="">Terms of Use</a>  and  Privacy Policy</p>
+                <div className='continue_with_email'><button className='flex'><span><img src={email} alt="" /></span>Continue with Email Id</button></div>
+                <p className='text-xs font-white-400'>By signing up, you agree to our <a href="">Terms of Use</a>  and  Privacy Policy</p>
             </div>
         </>
     );
